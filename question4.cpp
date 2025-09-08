c69e7401be388a28943536b1f15c5ecd316ac00c
ee250002083-pixel <ee250002083@iiti.ac.in>
2025-09-08 18:21:14 +0530

    Update question4.cpp

--- question4.cpp
+++ question4.cpp
@@ -4,9 +4,12 @@
 
 using namespace std;
 
-// TODO: Define TaylorTerm struct
-// struct TaylorTerm {
-// };
+// Define TaylorTerm struct
+struct TaylorTerm {
+    int order;      // the power of x in the term
+    double coeff;   // coefficient of the term
+    double value;   // actual value of the term for given x
+};
 
 void print_taylor_term(int index, int order, double coeff, double value) {
     cout << "Term[" << index << "]: "
@@ -21,6 +24,15 @@ void print_taylor_result(const char* function_name, double x,
          << "(x) at x=" << x
          << " with " << terms << " terms: "
          << approximation << "\n";
+}
+
+// Helper function to compute factorial
+double factorial(int n) {
+    double fact = 1.0;
+    for (int i = 2; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
 }
 
 int main(int argc, char* argv[]) {
@@ -39,17 +51,24 @@ int main(int argc, char* argv[]) {
     int terms;
     input >> x >> terms;
 
-    // TODO: Create an array of TaylorTerm
-    // TaylorTerm series[terms];
-
+    TaylorTerm series[100]; // max 100 terms to be safe
     double approximation = 0.0;
 
-    // TODO: Compute each Taylor term and store in array
-    // for (int k = 0; k < terms; k++) {
-    // }
+    for (int k = 0; k < terms; k++) {
+        int order = 2 * k + 1; // odd powers only
+        double coeff = pow(-1, k) / factorial(order);
+        double value = coeff * pow(x, order);
 
-    // TODO: Print final approximation
+        series[k] = {order, coeff, value};
 
+        approximation += value;
+
+        // print each term
+        print_taylor_term(k, series[k].order, series[k].coeff, series[k].value);
+    }
+
+    // print final approximation
+    print_taylor_result("sin", x, terms, approximation);
 
     return 0;
 }
